9c5510006bdd022e185a53f0c1cbe9c456d8b583
Dan Wright <dwrightco1@gmail.com>
2020-11-29 13:08:28 -0500

    Update configure-ntp.sh

--- vagrant/scripts/configure-ntp.sh
+++ vagrant/scripts/configure-ntp.sh
@@ -11,5 +11,12 @@ else
 	timezone="UTC"
 fi
 
+# install OS updates
+sudo apt-get update
+
+# set timezone
 sudo timedatectl set-timezone ${timezone}
+
+# ntp
 sudo timedatectl set-ntp on
+sudo apt install -y ntp
